0e8cc211bd69d7a5a6da460f2cc754281bfd47be
Ankitgaur657 <ankitg.ug23.cs@nitp.ac.in>
2025-02-07 21:00:19 +0530

    mongo db connection learning

---
+++ src/index.js
@@ -0,0 +1,35 @@
+//use try catch and async await to connect database
+//require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB();
+
+
+
+
+/*
+import express from "express";
+const app = express();
+(async()=>{
+    try {
+     await  mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}` )
+        console.log("Database connected successfully");
+        app.on("error",()=>{
+            console.log("Error while connecting to database");
+            throw error;
+        })
+
+      app.listen(process.env.PORT,()=>{
+          console.log(`Server is running on port ${process.env.PORT}`)
+      })
+    } catch (error) {
+        console.log("Error while connecting to database", error);
+        throw error;
+    }
+})()*///this is the same as below code
