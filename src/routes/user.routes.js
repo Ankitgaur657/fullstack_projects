11c01829c0764269abc02094d7d690aad4774dde
Ankitgaur657 <ankitg.ug23.cs@nitp.ac.in>
2025-02-13 16:16:21 +0530

    added new routes and Integrated aggregate pipelines

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,22 +1,45 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserchannelProfile,
+  getWatchHistroy,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        { name: "avatar", maxCount: 1 },
-        { name: "coverImage", maxCount: 1 }
-    ]),
-    registerUser
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser
 );
-//inject middlewares before functions that going to be execute 
-router.route("/login").post(loginUser)
+//inject middlewares before functions that going to be execute
+router.route("/login").post(loginUser);
 //securing routes-->
 
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar-update")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/coverImage-update")
+  .patch(verifyJWT, upload.single("/ccoverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserchannelProfile);
+router.route("/watchHistory").get(verifyJWT, getWatchHistroy);
 
-router.route("/logout").post(verifyJWT,logoutUser)
-router.route("/refresh").post(refreshAccessToken)
 export default router;
